5a97742386ecfbba287b301827d057965d745e3d
sashco mistelbacher <sashco.mistelbacher@gmail.com>
2025-05-18 13:03:23 -0400

    add tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(()=> {
+    const test = async () => {
+      await fetch("/api/explore/search?query=Keanu")
+    }
+
+    test()
+  })
 
   return (
     <>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   server: {
     port: 3000,
     // crash if port 3000 is not available
     strictPort: true,
     // make the host available outside of the docker container
     host: true,
+    // proxy requests to backend
+    proxy: {
+      '/api': {
+        target: 'http://backend:3001',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
   }
 })
